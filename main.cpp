ce52ec4137d4b9df0c1f126bb70a633cac098313
Xiao <xuantian19880401@gmail.com>
2018-06-26 14:08:00 -0400

    Client local update

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@
 #include <string>
 
 #define BUFFSIZE 64
-#define TIMEOUT_MS 1500
+#define TIMEOUT_MS 3500
 #define SERVERPORT 50025
 using namespace std;
 int main(int argc, char *argv[]) {
-    int fd;
+    int fd,streamfd;
     if(argc<5){
         perror("Invalid number of arguments");
         return 0;
@@ -36,8 +36,10 @@ int main(int argc, char *argv[]) {
         f=fopen(filename,"a");
 
     struct hostent *hp;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr, servudpaddr;
     socklen_t servsocklen=sizeof(servaddr);
+    socklen_t servudpsocklen=sizeof(servudpaddr);
+    int opt=1;
     unsigned char buf[BUFFSIZE];
     /*Set two timers for each session*/
     struct timeval tv1;
@@ -62,17 +64,23 @@ int main(int argc, char *argv[]) {
     servaddr.sin_port=htons(SERVERPORT);
     memcpy((void *)&servaddr.sin_addr,hp->h_addr_list[0], hp->h_length);
     char msg[BUFFSIZE];
-    if((fd=socket(AF_INET,SOCK_DGRAM,0))<0)
-        perror("Unable to start UDP scan socket");
+    if((streamfd=socket(AF_INET,SOCK_STREAM,0))<0){
+        perror("Unable to start TCP msg socket");
+        return -1;
+    }
+
+    if(connect(streamfd,(struct sockaddr*)&servaddr, sizeof(servaddr))<0){
+        perror("Server is not open");
+        return 0;
+    }
     sprintf(msg,"request:%d-%d",port,portend);
-    if((sendto(fd, msg, strlen(msg), 0, (struct sockaddr*)&servaddr, servsocklen))<0){
+    if((sendto(streamfd, msg, strlen(msg), 0, (struct sockaddr*)&servaddr, servsocklen))<0){
         perror("Message sending error");
         return 0;
     }
+    printf("Request sent:%d-%d\n",port,portend);
     memset(msg,0,BUFFSIZE);
-    if((setsockopt(fd,SOL_SOCKET, SO_RCVTIMEO,&tvmsg, sizeof(tvmsg)))<0)
-        perror("error setting recv timout");
-    if((recvfrom(fd,msg,BUFFSIZE,0,(struct sockaddr*)&servaddr,&servsocklen))<0){
+    if((recvfrom(streamfd,msg,BUFFSIZE,0,(struct sockaddr*)&servaddr,&servsocklen))<0){
         perror("Server ACK timeout.");
         return 0;
     }
@@ -83,67 +91,70 @@ int main(int argc, char *argv[]) {
     //begin scanning
     printf("Scanning begin:\n");
     char tofile[BUFFSIZE];
+    char* test=new char;
     bool inital=true;
     while(port<=portend){
-        if(port==SERVERPORT) {//avoid the server message port;
+        /*if(port==SERVERPORT) {//avoid the server message port;
             ++port;
             usleep(1500000);
             continue;
+        }*/
+        //hear from TCP:
+        memset(msg,0,BUFFSIZE);
+        if((recvfrom(streamfd,msg,BUFFSIZE,0,(struct sockaddr*)&servaddr,&servsocklen))<0){
+            perror("TCP message link broken during scan.\n");
+            return -1;
+        }
+        if(strcmp(msg,"end")==0){
+            fclose(f);
+            printf("Server says end.\n");
+            printf("Last scanned port %d.\n", port);
+            return 0;
+        }else{
+            port=atoi(msg);
+            if(port>portend||port<=0)
+                continue;
+            //printf("Scanning Port %d\n", port);
         }
         memset(buf,0,BUFFSIZE);
-        memset(&servaddr, 0, sizeof(servaddr));
-        servaddr.sin_family=AF_INET;
-        servaddr.sin_port=htons(port);
-        memcpy((void *)&servaddr.sin_addr,hp->h_addr_list[0], hp->h_length);
-        char* test="testmsg";
+        memset(&servudpaddr, 0, sizeof(servudpaddr));
+        servudpaddr.sin_family=AF_INET;
+        servudpaddr.sin_port=htons(port);
+        memcpy((void *)&servudpaddr.sin_addr,hp->h_addr_list[0], hp->h_length);
+
+        int testlen=0;
+        testlen=sprintf(test,"testmsg:%d",port);
 
         if((fd=socket(AF_INET,SOCK_DGRAM,0))<0)
             perror("Unable to start UDP socket");
-        if(inital){//initial wait
-            usleep(50000);
-            inital=false;
-        }
         if((setsockopt(fd,SOL_SOCKET, SO_RCVTIMEO,&tv1, sizeof(tv1)))<0)
             perror("error setting recv timout");
-        if((sendto(fd, test, strlen(test), 0, (struct sockaddr*)&servaddr, servsocklen))<0){
+        if((sendto(fd, test, testlen, 0, (struct sockaddr*)&servudpaddr, servudpsocklen))<0){
             printf("Port No.%d is not reachable.\n",port);
-            ++port;
+            //++port;
             close(fd);
             continue;
         }
-        if((recvfrom(fd,buf, BUFFSIZE, 0, (struct sockaddr*)&servaddr, &servsocklen))<=0){
-            if(errno==EAGAIN){
-                /*Try second wave*/
-                if((setsockopt(fd,SOL_SOCKET, SO_RCVTIMEO,&tv2, sizeof(tv2)))<0)
-                    perror("error setting recv timout at second attempt");
-                if((recvfrom(fd,buf, BUFFSIZE, 0, (struct sockaddr*)&servaddr, &servsocklen))<=0){
-                    printf("Port No.%d not responding second.\n",port);
-                }
-                else{
-                    goto suc;
-                }
-            }
-            else{
-                printf("Port No.%d not respongding.\n",port);
-                usleep(TIMEOUT_MS*2/3*1000);
-            }
-            /*Other error, no sleep?*/
-            ++port;
+        if((recvfrom(fd,buf, BUFFSIZE, 0, (struct sockaddr*)&servudpaddr, &servudpsocklen))<=0){
+            //++port;
             close(fd);
             continue;
 
         }
         /*printf("Received message:%s\n",buf);*/
+        //printf("Message in port %d: %s \n",port,buf);
         //printf("Port %d is alive.\n", port);
-suc:    memset(tofile,0,BUFFSIZE);
+        memset(tofile,0,BUFFSIZE);
         sprintf(tofile,"%d\n", port);
         fputs(tofile,f);
         close(fd);
-        ++port;
-        close(fd);
+        if(port==portend)
+            break;
         /*Some system has rate limitation*/
-        usleep(1200);
     }
+    delete test;
+    printf("Scanning Request Ends.\n");
     fclose(f);
+    close(streamfd);
     return 0;
 }
